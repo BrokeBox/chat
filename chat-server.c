8c278867edad3278903a46be91af58305b325585
Devin Trowbridge <devin.trow@gmail.com>
2021-07-05 11:12:03 -0500

    restructure and cleanup code

--- chat-server.c
+++ chat-server.c
@@ -7,114 +7,155 @@
 
 const char* welcome = "Welcome to chat! Type \"quit\" to exit.\n";
 
-int main(int argc , char* argv[])
+/*
+* Accept new client connections. 
+*/
+void accept_new_connection(SOCKET master_sock, SOCKET* clients) 
+{
+	int c = sizeof(struct sockaddr_in);
+	SOCKET new_socket;
+	struct sockaddr_in client;
+
+	if ((new_socket = accept(master_sock, (struct sockaddr*) &client, (int*) &c)) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%s:%d: Connected on socket %d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), new_socket);
+
+	// Send greeting
+	if (send(new_socket, welcome, strlen(welcome), 0) != strlen(welcome)) 
+		perror("send failed");
+
+	// Add to list of clients
+	FOR_EACH_CLIENT {
+		if (clients[i] == 0) {
+			clients[i] = new_socket;
+			break;
+		}
+	}
+}
+
+/*
+* Receive and log messages from clients and send them back out to all other clients. 
+*/
+void receive_client_messages(SOCKET* clients, const FD_SET* readfds) 
+{
+	char client_msg[MAX_MSG_LEN];
+	struct sockaddr_in client;
+	int c, recv_len;
+
+	// Otherwise its activity from an already connected client
+	for (int i = 0; (i < MAX_CLIENTS) && (clients[i] != 0); ++i) {
+		if(FD_ISSET(clients[i], readfds)) {
+			getpeername(clients[i] , (struct sockaddr*)&client , (int*)&c);
+
+			recv_len = recv(clients[i], client_msg, MAX_MSG_LEN, 0);
+
+			// Handle any disconnects first
+			if ((recv_len == SOCKET_ERROR) || (recv_len == 0)) {
+				printf("%s:d: Disconnected on socket %d\n" , inet_ntoa(client.sin_addr) , ntohs(client.sin_port), clients[i]);
+				closesocket( clients[i] );
+				clients[i] = 0;
+			} else { // Otherwise assume its a normal message
+				client_msg[recv_len] = '\0';
+				printf("%s:%d: %s\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), client_msg);
+				
+				// Send the message out to all clients...
+				for (int j = 0; (j < MAX_CLIENTS) && (clients[j] != 0); ++j) {
+					if (j != i) //...except the sender
+						send(clients[j] , client_msg , strlen(client_msg), 0);
+				}
+			}
+		}
+	}
+}
+
+/*
+* Setup sockets to prepare for client connections  
+*/
+int initialize(SOCKET* master_sock, SOCKET* clients)
 {
 	WSADATA wsa;
-	SOCKET sock, master_sock, new_socket, clients[MAX_CLIENTS];
-	struct sockaddr_in server, client;
-	int c, recv_len;
-    char* message;
-    char client_msg[MAX_MSG_LEN];
-    FD_SET readfds;
-
-    FOR_EACH_CLIENT{ clients[i] = 0; }
+	struct sockaddr_in server;
+	FOR_EACH_CLIENT{ clients[i] = 0; }
 
 	// Initialize WinSock
 	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-	{
+	if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
 		printf("Failed. Error Code : %d",WSAGetLastError());
 		return 1;
 	}
 	puts("Initialised.");
 
 	// Create master socket that will receive connections from clients
-	if ((master_sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+	if ((*master_sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
 		printf("Could not create socket : %d\n", WSAGetLastError());
-        return 1;
+		return 1;
 	}
 	puts("Master socket created.");
 
-    // Perpare socket sockaddr_in struct
+	// Perpare socket sockaddr_in struct
 	server.sin_addr.S_un.S_addr = INADDR_ANY;
 	server.sin_family = AF_INET;
 	server.sin_port = htons(8888);
 
-    // Bind socket
-    if (bind(master_sock, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
-        printf("Bind failed with error code: %d", WSAGetLastError());
-        SHUTDOWN(master_sock,1);
-    }
+	// Bind socket
+	if (bind(*master_sock, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+		printf("Bind failed with error code: %d", WSAGetLastError());
+		SHUTDOWN(*master_sock,1);
+	}
+}
 
-    // Listen for connections
-    listen(master_sock,3);
+/*
+* Main 
+*/
+int main(int argc , char* argv[])
+{
+	SOCKET master_sock, clients[MAX_CLIENTS];
+	
+	FD_SET readfds;
 
-    // Accept connection
-    puts("Waiting for incoming connection...");
+	int initcode = initialize(&master_sock, clients);
+	switch (initcode) {
+	case 1:
+		return 1;
+		break;
 
-    // Main execution loop
-    c = sizeof(struct sockaddr_in);
-    while (true) {
-        // Refresh the fd structure
-        FD_ZERO(&readfds);
-        FD_SET(master_sock, &readfds);
-        FOR_EACH_CLIENT {
-            if (clients[i] > 0) { FD_SET(clients[i], &readfds); }
-        }
+	case 2:
+		SHUTDOWN(master_sock, 1);
+		break;
 
-        // Wait for activity on a socket
-        select(0, &readfds, NULL, NULL, NULL);
+	default:
+		break;	
+	}
 
-        // Activity on master means a new connection coming in
-        if (FD_ISSET(master_sock, &readfds)) {
-            if ((new_socket = accept(master_sock , (struct sockaddr *)&client, (int *)&c))<0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
+	// Listen for connections
+	listen(master_sock,3);
 
-            printf("%s:%d: Connected on socket %d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), new_socket);
-            
-            // Send greeting
-            if (send(new_socket, welcome, strlen(welcome), 0) != strlen(welcome)) { perror("send failed"); }
+	// Accept connection
+	puts("Waiting for incoming connection...");
 
-            // Add to list of clients
-            FOR_EACH_CLIENT {
-                if (clients[i] == 0) {
-                    clients[i] = new_socket;
-                    break;
-                }
-            }
-        } 
+	// Main execution loop
+	
+	while (true) {
+		// Refresh the fd structure
+		FD_ZERO(&readfds);
+		FD_SET(master_sock, &readfds);
+		FOR_EACH_CLIENT {
+			if (clients[i] > 0) { FD_SET(clients[i], &readfds); }
+		}
 
-        // Otherwise its activity from an already connected client
-        for (int i = 0; (i < MAX_CLIENTS) && (clients[i] != 0); ++i) {
-            if(FD_ISSET(clients[i], &readfds)) {
-                getpeername(clients[i] , (struct sockaddr*)&client , (int*)&c);
+		// Wait for activity on a socket
+		select(0, &readfds, NULL, NULL, NULL);
 
-                recv_len = recv(clients[i], client_msg, MAX_MSG_LEN, 0);
+		// Activity on master means a new connection coming in
+		if (FD_ISSET(master_sock, &readfds)) {
+			accept_new_connection(master_sock, clients);
+		}
 
-                // Handle any disconnects first
-                if ((recv_len == SOCKET_ERROR) || (recv_len == 0)) {
-                    //Somebody disconnected , get his details and print
-                    printf("%s:d: Disconnected on socket %d\n" , inet_ntoa(client.sin_addr) , ntohs(client.sin_port), clients[i]);
-                    
-                    //Close the socket and mark as 0 in list for reuse
-                    closesocket( clients[i] );
-                    clients[i] = 0;
-                }
-                else { // Otherwise assume its a normal message
-                    client_msg[recv_len] = '\0';
-                    printf("%s:%d: %s\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), client_msg);
-                    // Send the message out to all clients...
-                    for (int j = 0; (j < MAX_CLIENTS) && (clients[j] != 0); ++j) {
-                        if (j != i) { //...except the sender
-                            send(clients[j] , client_msg , strlen(client_msg), 0);
-                        }
-                    }
-                }
-            }
-        }
-    }
+		receive_client_messages(clients, &readfds);
+	}
 
-    SHUTDOWN(master_sock,0);
+	SHUTDOWN(master_sock, 0);
 }
--- chat.c
+++ chat.c
@@ -4,74 +4,89 @@
 
 #include "Helpers.h"
 
-int main(int argc , char* argv[])
+int initialize(SOCKET* sock)
 {
 	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char message[MAX_MSG_LEN], input[MAX_MSG_LEN], server_reply[MAX_MSG_LEN], name[100];
-    unsigned int recv_size;
 	u_long mode = 1;  // 1 to enable non-blocking socket
-
-    printf( "username:");
-    scanf("%s", name);
+	struct sockaddr_in server;
 
 	// Initialize WinSock
 	printf("\nInitialising...");
-	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-	{
+	if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
 		printf("Failed. Error Code : %d",WSAGetLastError());
 		return 1;
 	}
 
 	// Create socket
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+	if ((*sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
 		printf("Could not create socket : %d\n", WSAGetLastError());
-        return 1;
+		return 1;
 	}
 
-    // Connect socket to server
+	// Connect socket to server
 	server.sin_addr.S_un.S_addr = inet_addr("192.168.0.20");
 	server.sin_family = AF_INET;
 	server.sin_port = htons(8888);
-	if (connect(s, (struct sockaddr*)&server, sizeof(server)) < 0) {
+	if (connect(*sock, (struct sockaddr*) &server, sizeof(server)) < 0) {
 		puts("connect error");
-		SHUTDOWN(s,1)
+		return 2;
 	}
 	puts("Connected");
 
-	ioctlsocket(s, FIONBIO, &mode); // Make socket non-blocking
+	ioctlsocket(*sock, FIONBIO, &mode); // Make socket non-blocking
+}
 
-    char firstLoop = 1;
+int main(int argc , char* argv[])
+{
+	SOCKET sock;
+	char message[MAX_MSG_LEN], input[MAX_MSG_LEN], server_reply[MAX_MSG_LEN], name[100];
+	unsigned int recv_size;
+	
+	int initcode = initialize(&sock);
+	switch (initcode) {
+	case 1:
+		return 1;
+		break;
 
-    // Main execution loop
-    while (true){
-        // Get user input
+	case 2:
+		SHUTDOWN(sock, 1);
+		break;
+
+	default:
+		break;	
+	}
+
+	printf("username:");
+	scanf("%s", name);
+
+	// Main execution loop
+	while (true){
+		// Get user input
 		if (_kbhit()){
-        	gets(input);
-            if (strlen(input) > 0) {
-                // Quit gracefully
-                if (strcmp(input, "quit") == 0) {
-                    send(s, "", strlen(0), 0); // Let the server know we're disconnecting
-                    SHUTDOWN(s,0)
-                }
+			gets(input);
+			if (strlen(input) > 0) {
+			// Quit gracefully
+				if (strcmp(input, "quit") == 0) {
+					send(sock, "", strlen(0), 0); // Let the server know we're disconnecting
+					SHUTDOWN(sock,0)
+				}
 
-                // Send message to server
-                sprintf(message, "%s: %s\0", name, input);
-                if (send(s, message, strlen(message), 0) < 0) {
-                    puts("Send failed");
-                }
-                printf("%s: ", name);
-            }
+				// Send message to server
+				sprintf(message, "%s: %s\0", name, input);
+				if (send(sock, message, strlen(message), 0) < 0) {
+					puts("Send failed");
+				}
+				printf("%s: ", name);
+			}
 		}
 
-        // Get server traffic
-		if ((recv_size = recv(s, server_reply, MAX_MSG_LEN, 0)) != SOCKET_ERROR) {
-            server_reply[recv_size] = '\0';
+		// Get server traffic
+		if ((recv_size = recv(sock, server_reply, MAX_MSG_LEN, 0)) != SOCKET_ERROR) {
+			server_reply[recv_size] = '\0';
 			printf("\r%s\n", server_reply);
-            printf("%s: ", name);
+			printf("%s: ", name);
 		}
 	}
 
-    SHUTDOWN(s,0)
+	SHUTDOWN(sock,0)
 }
